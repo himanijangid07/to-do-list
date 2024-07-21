101b7e27df8dfeb275e08f87651c3c1d9df7abc9
himanijangid07 <himanijangid07@gmail.com>
2024-07-21 20:25:26 +0530

    Added files for to-do list

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementsByClassName("list");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        
+    }
+    inputBox.value = "";
+}
